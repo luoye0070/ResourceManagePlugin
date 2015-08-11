7ea00ec14297b9d04d7375a5f6f381a1e4b236c9
luoye_lj <luoye.lj@sohu.com>
2015-08-11 21:46:17 +0800

    添加说明和打包排除jar包

--- ResourceManagePluginGrailsPlugin.groovy
+++ ResourceManagePluginGrailsPlugin.groovy
@@ -1,11 +1,12 @@
 class ResourceManagePluginGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-            "grails-app/views/error.gsp"
+            "grails-app/views/error.gsp",
+            "lib/**"
     ]
 
     // TODO Fill in these fields
